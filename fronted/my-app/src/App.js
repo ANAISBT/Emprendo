7cbc42f50611fea854c2e2a8c4533f79913c6c1b
Anais Milagros <anais.bustamante@paku.pe>
2024-10-04 01:17:01 -0500

    Register-Emprendedor

--- fronted/my-app/src/App.js
+++ fronted/my-app/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Register from "./pages/Register-Emprendedor";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
