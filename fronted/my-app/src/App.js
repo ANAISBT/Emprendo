f6c7f744be7a666c2e0ba519b473cca57da1cd19
Anais Milagros <anais.bustamante@paku.pe>
2024-10-03 23:55:15 -0500

    Login

--- fronted/my-app/src/App.js
+++ fronted/my-app/src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
+import Login from "./pages/Login";
 import Home from "./pages/Home";
 
 function App() {
