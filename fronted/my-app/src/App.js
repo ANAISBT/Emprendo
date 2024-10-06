799abe2d9e51473410f6eefc6f1413d49c67c05c
Anais Milagros <anais.bustamante@paku.pe>
2024-10-06 15:04:22 -0500

    complete Registers

--- fronted/my-app/src/App.js
+++ fronted/my-app/src/App.js
@@ -2,16 +2,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Home from "./pages/Home";
 import RegisterEmprendedor from "./pages/Register-Emprendedor";
+import RegisterUsuario from "./pages/RegisterUsuario";
+import TipoPerfil from "./pages/TipoPerfil";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/registerEmprendedor" element={<RegisterEmprendedor />} />
+        <Route path="/registerUsuario" element={<RegisterUsuario />} />
+        <Route path="/rolSelect" element={<TipoPerfil />} />
       </Routes>
     </Router>
   );
